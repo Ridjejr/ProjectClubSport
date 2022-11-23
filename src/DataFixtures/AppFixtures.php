b4aa7b7d372b71c8c2e4e1c85cf02d900133ec58
Ridjejr <esdrasmissilou@gmail.com>
2022-11-23 11:26:32 +0100

    page d'accueil

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,15 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker=Factory::create("fr_FR");
 
         $manager->flush();
     }
